5f802d9b565e46c33280575b33d4f89d9ca271ca
unknown <chandru151998@gmail.com>
2023-01-06 02:36:19 +0530

    [Chandru] Added. Viewsets

--- student/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from student import views
-
-urlpatterns = [
-    path('lc/', views.StudentListCreate.as_view()),
-    path('rud/<int:pk>/', views.StudentRetrieveUpdateDestroy.as_view()),
-]
