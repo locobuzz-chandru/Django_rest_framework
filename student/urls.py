668ae8808ccab0c0aafea78b0070bd2a433b4bfd
unknown <chandru151998@gmail.com>
2023-01-06 00:15:00 +0530

    [Chandru] Added. Mixins and GenericAPIView

--- student/urls.py
+++ student/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from student import views
 
 urlpatterns = [
-    path('student_list/', views.StudentList.as_view()),
-    path('student_create/', views.StudentCreate.as_view()),
-    path('student_retrieve/<int:pk>/', views.StudentRetrieve.as_view()),
-    path('student_update/<int:pk>/', views.StudentUpdate.as_view()),
-    path('student_destroy/<int:pk>/', views.StudentDestroy.as_view()),
+    path('lc/', views.LCapi.as_view()),
+    path('rud/<int:pk>/', views.RUDapi.as_view()),
 ]
