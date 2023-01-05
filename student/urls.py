d49c453d91ad0e84b952fae304bdddc5f773df80
unknown <chandru151998@gmail.com>
2023-01-06 01:24:51 +0530

    [Chandru] Added. Concrete View combined

--- student/urls.py
+++ student/urls.py
@@ -3,7 +3,5 @@ from student import views
 
 urlpatterns = [
     path('lc/', views.StudentListCreate.as_view()),
-    path('ru/<int:pk>/', views.StudentRetrieveUpdate.as_view()),
-    path('rd/<int:pk>/', views.StudentRetrieveDestroy.as_view()),
     path('rud/<int:pk>/', views.StudentRetrieveUpdateDestroy.as_view()),
 ]
--- student/views.py
+++ student/views.py
@@ -1,19 +1,9 @@
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class StudentListCreate(ListCreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentRetrieveUpdate(RetrieveUpdateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-
-
-class StudentRetrieveDestroy(RetrieveDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
