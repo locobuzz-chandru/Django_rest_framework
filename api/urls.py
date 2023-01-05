29f006f456445ec53c7c9e15684fdd1a3d5656db
unknown <chandru151998@gmail.com>
2023-01-06 02:56:19 +0530

    [Chandru] Added. Read Only Model View Set

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
-from student.views import StudentModelViewSet
+from student.views import StudentReadOnlyModelViewSet
 from rest_framework.routers import DefaultRouter
 
 # Creating Router Object
 router = DefaultRouter()
 
 # Register StudentViewSet with Router
-router.register('studentapi', StudentModelViewSet, basename='student')
+router.register('studentapi', StudentReadOnlyModelViewSet, basename='student')
 
 urlpatterns = router.urls
--- student/views.py
+++ student/views.py
@@ -3,6 +3,6 @@ from .serializers import StudentSerializer
 from rest_framework import viewsets
 
 
-class StudentModelViewSet(viewsets.ModelViewSet):
+class StudentReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
