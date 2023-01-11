cf1b3ae1b7de4fcd3c58a79c56aa772ff1311f0e
unknown <chandru151998@gmail.com>
2023-01-11 20:28:41 +0530

    [Chandru]Added. Session authentication

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path, include
+
 from student.views import StudentModelViewSet, SchoolModelViewSet
 from rest_framework.routers import DefaultRouter
 
@@ -9,4 +11,7 @@ router.register('student', StudentModelViewSet, basename='student')
 router.register('school', SchoolModelViewSet, basename='school')
 
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- student/views.py
+++ student/views.py
@@ -1,8 +1,9 @@
 from .models import Student, School
 from .serializers import StudentSerializer, SchoolSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, \
+    DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
@@ -15,5 +16,10 @@ class StudentModelViewSet(viewsets.ModelViewSet):
 class SchoolModelViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAdminUser]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [DjangoModelPermissions]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
