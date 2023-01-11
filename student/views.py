8a5c14683c543a68def214cc4c71293987763895
unknown <chandru151998@gmail.com>
2023-01-11 22:36:58 +0530

    [Chandru]Added. Custom authentication

--- student/views.py
+++ student/views.py
@@ -1,9 +1,9 @@
+from rest_framework.permissions import AllowAny
 from .models import Student, School
 from .serializers import StudentSerializer, SchoolSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, \
-    DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from .custompermission import MyPermission
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
@@ -17,9 +17,4 @@ class SchoolModelViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
     authentication_classes = [SessionAuthentication]
-    # permission_classes = [AllowAny]
-    # permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAdminUser]
-    # permission_classes = [IsAuthenticatedOrReadOnly]
-    # permission_classes = [DjangoModelPermissions]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
